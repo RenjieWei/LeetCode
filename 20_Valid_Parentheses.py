fb0b912705020ec8f93588542b900064e331a1db
Renjie Wei <renjie550201@gmail.com>
2019-09-08 22:41:48 -0400

    add 1.Two Sum

---
+++ 20_Valid_Parentheses.py
@@ -0,0 +1,26 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        
+        if len(s)%2 !=0:
+            return False
+        
+        opening = set('{[(')
+        matches = set([('(',')'),('[',']'),('{','}')])
+        stack = []
+        
+        for paren in s:
+            if paren in opening:
+                stack.append(paren)
+            
+            else:
+                if len(stack)==0:
+                    return False
+                
+                last_open = stack.pop()
+                
+                if (last_open,paren) not in matches:
+                    return False
+                
+        return len(stack) == 0
+    
+   
