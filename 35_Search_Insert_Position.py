0deefc183f1ca3c7d268150429dc1e18c1560687
Renjie Wei <42357472+RenjieWei@users.noreply.github.com>
2019-09-26 00:20:30 -0400

    Create 35_Search_Insert_Position.py

---
+++ 35_Search_Insert_Position.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        left = 0
+        right = len(nums)-1
+        while left <= right:
+            mid = (right - left) / 2 + left
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                right = mid - 1
+            else:
+                left = mid + 1
+        return left
