8c980776551cb58544e721a87a47cfd15b6f8bcb
Renjie Wei <42357472+RenjieWei@users.noreply.github.com>
2019-09-26 14:58:17 -0400

    Update 189_Rotate_Array.py

---
+++ 189_Rotate_Array.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        nums[:] = nums[n-k:] + nums[:k+1]
+        
