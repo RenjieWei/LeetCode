da700247e1ec9b0b0dea3e483db69b331293229c
Renjie Wei <42357472+RenjieWei@users.noreply.github.com>
2019-09-26 15:24:24 -0400

    Create 217_Contains_Duplicate.py

---
+++ 217_Contains_Duplicate.py
@@ -0,0 +1,18 @@
+# Method 1
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        #hash table
+        mydict = {}
+        result = False
+        for index, num in enumerate(nums):
+            if num in mydict:
+                result = True
+                
+            mydict[num] = 1
+        return result
+          
+  
