0929698f1c0d49005c3de89a3fa0bba254d3a672
Renjie Wei <42357472+RenjieWei@users.noreply.github.com>
2019-09-26 12:05:08 -0400

    Update 121_Best_Time_to_Buy_and_Sell_Stock.py

--- 121_Best_Time_to_Buy_and_Sell_Stock.py
+++ 121_Best_Time_to_Buy_and_Sell_Stock.py
@@ -1,1 +1,12 @@
-d
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        curPro = maxPro = 0
+        for i in range(1,len(prices)):
+            curPro = max(0,curPro + prices[i] - prices[i-1])
+            maxPro = max(curPro,maxPro)
+        return maxPro
+        
