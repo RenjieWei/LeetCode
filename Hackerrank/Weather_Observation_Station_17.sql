477ac9838e806c584f8a410f65ea3e24f7f50c41
Renjie Wei <42357472+RenjieWei@users.noreply.github.com>
2019-10-16 19:30:25 -0400

    Create Weather_Observation_Station_17.sql

---
+++ Hackerrank/Weather_Observation_Station_17.sql
@@ -0,0 +1,7 @@
+-- Query the Western Longitude (LONG_W)where the smallest Northern Latitude (LAT_N) in STATION is greater than .
+-- Round your answer to 4 decimal places.
+select round(long_w,4)
+from station
+where lat_n = (select min(lat_n) 
+              from station
+              where lat_n > 38.7780);
