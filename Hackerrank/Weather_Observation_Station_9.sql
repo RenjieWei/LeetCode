e2264a610fdd88fb40017c6908c2a6709c1b51dc
Renjie Wei <42357472+RenjieWei@users.noreply.github.com>
2019-10-16 14:34:06 -0400

    Create Weather_Observation_Station_9.sql

---
+++ Hackerrank/Weather_Observation_Station_9.sql
@@ -0,0 +1,3 @@
+-- Query the list of CITY names from STATION that do not start with vowels. Your result cannot contain duplicates.
+select distinct city from station
+where city NOT REGEXP '^[aeiou]';
