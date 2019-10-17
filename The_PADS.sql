8d24c53b4215186a7d364646494d31a08a920d75
Renjie Wei <42357472+RenjieWei@users.noreply.github.com>
2019-10-17 00:26:28 -0400

    Create The_PADS.sql

---
+++ The_PADS.sql
@@ -0,0 +1,15 @@
+-- Generate the following two result sets:
+
+-- Query an alphabetically ordered list of all names in OCCUPATIONS, immediately followed by the first letter of each profession as a parenthetical (i.e.: enclosed in parentheses). For example: AnActorName(A), ADoctorName(D), AProfessorName(P), and ASingerName(S).
+-- Query the number of ocurrences of each occupation in OCCUPATIONS. Sort the occurrences in ascending order, and output them in the following format:
+
+-- There are a total of [occupation_count] [occupation]s.
+
+select concat(Name,'(',substring(Name,1,1),')') 
+from occupations 
+order by Name;
+
+select concat('There are a total of ',count(*),' ',LOWER(Occupation), 's.') 
+ from occupations 
+group by Occupation
+order by count(Occupation), Occupation;
