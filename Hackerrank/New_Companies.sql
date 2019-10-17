7c738880c3bd0660a5e224d12189b7259659d3ea
Renjie Wei <42357472+RenjieWei@users.noreply.github.com>
2019-10-17 14:56:26 -0400

    Update New_Companies.sql

--- Hackerrank/New_Companies.sql
+++ Hackerrank/New_Companies.sql
@@ -1,3 +1,4 @@
+-- Tedious solution
 select c.company_code, c.founder,t1.n_lead,t2.n_senior,t3.n_manager, t4.n_employee
 from 
 (select company_code, founder
@@ -24,3 +25,15 @@ join
 group by company_code 
 order by company_code) t4
 on t3.company_code = t4.company_code
+
+-- concise solution
+SELECT c.company_code, c.founder, 
+COUNT(DISTINCT l.lead_manager_code), COUNT(DISTINCT s.senior_manager_code),
+ COUNT(DISTINCT m.manager_code), COUNT(DISTINCT e.employee_code)
+FROM Company c, Lead_Manager l, Senior_Manager s, Manager m, Employee e
+WHERE c.company_code = l.company_code AND 
+      l.lead_manager_code = s.lead_manager_code AND
+      s.senior_manager_code = m.senior_manager_code AND
+      m.manager_code = e.manager_code
+GROUP BY c.company_code, c.founder 
+ORDER BY c.company_code;
